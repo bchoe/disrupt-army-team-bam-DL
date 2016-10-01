0e28994c1d57df2c56e4cf6e8dc1115e504de8f2
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-10-01 00:40:39 -1000

    stubbed out a bar graph in fakeGraphs, adjusted styling further

--- fakeGraphs.js
+++ fakeGraphs.js
@@ -1,4 +1,33 @@
 console.log('works');
+
+//single bar graph
+/*
+let container = document.querySelector(".visualizer");
+
+container.innerHTML = "";
+var barChart = c3.generate({
+    data: {
+        columns: [
+            ['data1', -30, 200, 200, 400, -150, 250],
+            ['data2', 130, 100, -100, 200, -150, 50],
+            ['data3', -230, 200, 200, -300, 250, 250]
+        ],
+        type: 'bar',
+        groups: [
+            ['data1', 'data2', 'data3']
+        ]
+    },
+    grid: {
+        y: {
+            lines: [{value:0}]
+        }
+    }
+});
+container.appendChild(barChart.element);
+
+*/
+//individual graphs
+
 let graphs = document.querySelectorAll(".dataViz");
 
 graphs = Array.from(graphs);
@@ -8,8 +37,14 @@ function applyGraph(element, index, array) {
   var chart = c3.generate({
     data: {
       columns: [
-        ['data1', 30],
-        ['data2', 120],
+        ['NATO', 30],
+        ['Allies', 120],
+        ['Army', 48],
+        ['Officials', 65],
+        ['Supporters', 90],
+        ['Joint', 25],
+        ['Staff', 35],
+        ['free time', 75]
       ],
       type : 'donut',
       onclick: function (d, i) { console.log("onclick", d, i); },
@@ -17,9 +52,10 @@ function applyGraph(element, index, array) {
       onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     },
     donut: {
-        title: "Iris Petal Width"
+        title: "CG"
     }
   });
+
   element.appendChild(chart.element);
 }
 
